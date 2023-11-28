3102c2abb48925ceb28facd14c1deac0468a9626
raihan0504 <124858485+raihan0504@users.noreply.github.com>
2023-11-28 11:15:08 +0700

    Create Akun.java

---
+++ Akun.java
@@ -0,0 +1,19 @@
+class Akun {
+    private String id;
+
+    public Akun(){
+
+    }
+
+    public Akun(String id){
+        this.id = id;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
