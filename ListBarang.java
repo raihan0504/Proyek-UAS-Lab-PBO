6e65e735ea0659220cccc174436496790d49d203
raihan0504 <124858485+raihan0504@users.noreply.github.com>
2023-11-30 15:55:50 +0700

    Update ListBarang.java

--- ListBarang.java
+++ ListBarang.java
@@ -9,36 +9,14 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ListBarang {
-    private final String FILE_NAME = "barang.txt";
+    protected ArrayList<Barang> barang;
+    protected final String FILE_NAME = "Barang.txt";
 
-    public void tambahBarang(){
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Masukkan kode barang: ");
-        String kode = scan.next();
-        System.out.print("Masukkan nama barang: ");
-        String nama = scan.next();
-        System.out.print("Masukkan harga barang; ");
-        int harga = scan.nextInt();
-
-        try {
-            File file = new File (FILE_NAME);
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-
-            FileWriter tulisFile = new FileWriter(file, true);
-            BufferedWriter bufferWriter = new BufferedWriter(tulisFile);
-            bufferWriter.write(kode + "," + nama + "," + harga);
-            bufferWriter.newLine();
-            bufferWriter.close();
-            System.out.println("Barang berhasi ditambahkan!");
-        } catch (IOException e){
-            System.out.println("Terjadi kesalahan saat menambahkan barang!");
-            e.printStackTrace();
-        }
+    public ListBarang() {
+        barang = new ArrayList<>();
     }
 
-    public void editBarang(){
+    public void editBarang() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Masukkan kode barang yang ingin di edit: ");
         String kode = scan.next();
@@ -53,7 +31,7 @@ public class ListBarang {
                 file.createNewFile();
             }
 
-            List<String> lines = new ArrayList<>();
+            List<String> barang = new ArrayList<>();
             BufferedReader reader = new BufferedReader(new FileReader(file));
             String line;
             while ((line = reader.readLine()) != null) {
@@ -61,13 +39,12 @@ public class ListBarang {
                 if (part[0].equals(kode)) {
                     line = kode + "," + nama + "," + harga;
                 }
-                lines.add(line);
+                barang.add(line);
             }
-            reader.close();
 
             FileWriter fileWriter = new FileWriter(file);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            for (String l : lines) {
+            for (String l : barang) {
                 bufferedWriter.write(l);
                 bufferedWriter.newLine();
             }
@@ -79,58 +56,90 @@ public class ListBarang {
         }
     }
 
-    public void hapusBarang(){
+    public void hapusBarang() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Masukkan kode barang yang ingin dihapus: ");
         String kode = scan.next();
-        
+
         try {
             File file = new File(FILE_NAME);
-            if (!file.exists()){
+            if (!file.exists()) {
                 file.createNewFile();
             }
-            
-            List<String> lines = new ArrayList<>();
+
+            List<String> barang = new ArrayList<>();
             BufferedReader reader = new BufferedReader(new FileReader(file));
             String line;
-            while((line=reader.readLine())!=null){
+            while ((line = reader.readLine()) != null) {
                 String[] part = line.split(",");
-                if(!part[0].equals(kode)){
-                    lines.add(line);
+                if (!part[0].equals(kode)) {
+                    barang.add(line);
                 }
             }
             reader.close();
 
             FileWriter fileWriter = new FileWriter(file);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            for (String l : lines) {
+            for (String l : barang) {
                 bufferedWriter.write(l);
                 bufferedWriter.newLine();
             }
             bufferedWriter.close();
             System.out.println("Barang berhasil dihapus!");
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Terjadi kesalahan saat menghapus barang!");
             e.printStackTrace();
         }
     }
 
-    public void tampilBarang(){
+    public void tampilBarang() {
         try {
             File file = new File(FILE_NAME);
-            if (!file.exists()){
+            if (!file.exists()) {
                 file.createNewFile();
             }
 
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                String[] part = line.split(","); 
-                System.out.println("kode: " + part[0] + ", nama: " + part[1] + ", harga: " + part[2]);
+                String[] part = line.split(",");
+                if (part.length >= 3) {
+                    System.out.println("Kode: " + part[0]);
+                    System.out.println("Nama: " + part[1]);
+                    System.out.println("Harga: " + part[2]);
+                    System.out.println(); // Menambahkan baris kosong sebagai pemisah
+                } else {
+                    System.out.println("Format tidak valid: " + line);
+                }
             }
             bufferedReader.close();
         } catch (IOException e) {
             System.out.println("Terjadi kesalahan saat menampilkan barang!");
+            e.printStackTrace();
+        }
+    }
+
+    public void tambahBarang() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukkan kode barang: ");
+        String kode = scan.nextLine();
+        System.out.print("Masukkan nama barang: ");
+        String nama = scan.nextLine();
+        System.out.print("Masukkan harga barang: ");
+        int harga = scan.nextInt();
+
+        try {
+            File file = new File(FILE_NAME);
+
+            FileWriter tulisFile = new FileWriter(file, true);
+            BufferedWriter bufferWriter = new BufferedWriter(tulisFile);
+            bufferWriter.write(kode + "," + nama + "," + harga);
+            bufferWriter.newLine();
+            bufferWriter.close();
+            System.out.println("Barang berhasi ditambahkan!");
+        } catch (IOException e) {
+            System.out.println("Terjadi kesalahan saat menambahkan barang!");
+            e.printStackTrace();
         }
     }
 }
