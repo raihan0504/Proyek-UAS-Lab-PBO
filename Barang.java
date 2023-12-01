6a11d41a7432669ecd82f53e2aec3c8bee7ff047
dellarahmatika <128202610+dellara22@users.noreply.github.com>
2023-12-01 11:21:59 +0700

    Add files via upload

--- Barang.java
+++ Barang.java
@@ -1,24 +1,33 @@
 public class Barang {
+    private String kodeBarang;
     private String namaBarang;
     private int hargaBarang;
     private int stokBarang;
 
-    public Barang(String namaBarang, int hargaBarang, int stokBarang){
+    public Barang(String kodeBarang, String namaBarang, int hargaBarang, int stokBarang){
+        this.kodeBarang = kodeBarang;
         this.namaBarang = namaBarang;
         this.hargaBarang = hargaBarang;
         this.stokBarang = stokBarang;
     }
-
+    public String getKodeBarang() {
+        return kodeBarang;
+    }
+    
     public int getHargaBarang() {
         return hargaBarang;
     }
-
+    
     public String getNamaBarang() {
         return namaBarang;
     }
-
+    
     public int getStokBarang() {
         return stokBarang;
+    }
+
+    public void setKodeBarang(String kodeBarang) {
+        this.kodeBarang = kodeBarang;
     }
 
     public void setNamaBarang(String namaBarang) {
