4beb117b2d9cec281a6cd7f2c1cef90b4dc7bdd8
raihan0504 <124858485+raihan0504@users.noreply.github.com>
2023-12-02 14:27:00 +0700

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,33 +1,4 @@
-/**
- * Abstrak class driver yang menghubungkan login admin dan customer
- */
 public abstract class Driver {
-    protected CustomerDriver customerDriver;
-    protected AdminDriver adminDriver;
-
-    /**
-     * Konstruktor untuk kelas driver
-     * 
-     * @param customerDriver
-     * @param adminDriver
-     */
-    public Driver(CustomerDriver customerDriver, AdminDriver adminDriver){
-        this.customerDriver = customerDriver;
-        this.adminDriver = adminDriver;
-    }
-
-    /**
-     * Metode abstrak untuk login customer
-     * 
-     * @param id    Id customer
-     * @param password Password customer
-     */
-    public abstract void customerLogin(String id, String password);
-    /**
-     * Metode abstrak untuk login admin
-     * 
-     * @param id    Id admin
-     * @param password  Password admin
-     */
-    public abstract void adminLogin(String id, String password);
-} 
+    
+    public abstract void Menu();
+}
