26571e5bf367a1c1aa8ee6505ae353f34ef26f68
raihan0504 <124858485+raihan0504@users.noreply.github.com>
2023-12-04 01:51:15 +0700

    Update ListBarang.java

--- ListBarang.java
+++ ListBarang.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
@@ -5,19 +6,37 @@ import java.util.Scanner;
 public class ListBarang {
     private List<Barang> daftarBarang;
 
-    public ListBarang(){
-
+    /**
+     * Konstruktor untuk membuat objek ListBarang dengan menginisialisasi daftarBarang
+     * dari data yang dibaca dari file "Barang.txt".
+     */
+    public ListBarang() {
+        this.daftarBarang = new ArrayList<>();
+        this.daftarBarang = Barang.bacaDariFile("Barang.txt");
     }
 
+    /**
+     * Konstruktor alternatif untuk membuat objek ListBarang dengan daftarBarang yang
+     * diberikan sebagai parameter.
+     *
+     * @param daftarBarang Daftar barang yang akan digunakan.
+     */
     public ListBarang(List<Barang> daftarBarang) {
         this.daftarBarang = daftarBarang;
     }
 
+    /**
+     * Mendapatkan daftar barang.
+     *
+     * @return Daftar barang.
+     */
     public List<Barang> getDaftarBarang() {
         return daftarBarang;
     }
 
-
+    /**
+     * Menambahkan barang baru ke daftarBarang.
+     */
     public void tambahBarang() {
         Scanner scan = new Scanner(System.in);
 
@@ -27,21 +46,35 @@ public class ListBarang {
         String nama = scan.nextLine();
         System.out.print("Masukkan harga barang: ");
         int harga = scan.nextInt();
+        scan.nextLine(); // Consume the newline character
         System.out.print("Masukkan stok barang: ");
         int stok = scan.nextInt();
+        scan.nextLine(); // Consume the newline character
+
         // Buat objek Barang baru
         Barang barangBaru = new Barang(kode, nama, harga, stok);
         // Tambahkan objek Barang baru ke dalam daftarBarang
         daftarBarang.add(barangBaru);
 
         System.out.println("Barang berhasil ditambahkan!");
+
+        // Save the changes to the file
+        simpanKeFile("Barang.txt");
     }
-    // Metode untuk menyimpan perubahan ke dalam file
+
+    /**
+     * Menyimpan perubahan daftarBarang ke file.
+     *
+     * @param namaFile Nama file tempat menyimpan data barang.
+     */
     public void simpanKeFile(String namaFile) {
         Barang.simpanKeFile(daftarBarang, namaFile);
         System.out.println("Daftar barang berhasil disimpan ke file: " + namaFile);
     }
 
+    /**
+     * Menghapus barang dari daftarBarang berdasarkan kode barang.
+     */
     public void hapusBarang() {
         Scanner scan = new Scanner(System.in);
 
@@ -54,6 +87,7 @@ public class ListBarang {
             if (barang.getKodeBarang().equals(kode)) {
                 iterator.remove();
                 System.out.println("Barang berhasil dihapus!");
+                simpanKeFile("Barang.txt");
                 return;
             }
         }
@@ -61,15 +95,16 @@ public class ListBarang {
         System.out.println("Barang dengan kode " + kode + " tidak ditemukan.");
     }
 
+    /**
+     * Mengedit data barang dalam daftarBarang berdasarkan kode barang.
+     */
     public void editBarang() {
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Masukkan kode barang yang akan diedit: ");
         String kode = scan.nextLine();
 
-        Iterator<Barang> iterator = daftarBarang.iterator();
-        while (iterator.hasNext()) {
-            Barang barang = iterator.next();
+        for (Barang barang : daftarBarang) {
             if (barang.getKodeBarang().equals(kode)) {
                 System.out.println("Data barang yang akan diubah:");
                 System.out.println("Nama Barang: " + barang.getNamaBarang());
@@ -79,16 +114,16 @@ public class ListBarang {
                 System.out.print("Masukkan nama barang baru: ");
                 String namaBaru = scan.nextLine();
                 System.out.print("Masukkan harga barang baru: ");
-                int hargaBaru = scan.nextInt();
+                int hargaBaru = Integer.parseInt(scan.nextLine());
                 System.out.print("Masukkan stok barang baru: ");
-                int stokBaru = scan.nextInt();
+                int stokBaru = Integer.parseInt(scan.nextLine());
 
-                // Update data barang
                 barang.setNamaBarang(namaBaru);
                 barang.setHargaBarang(hargaBaru);
                 barang.setStokBarang(stokBaru);
 
                 System.out.println("Barang berhasil diubah!");
+                simpanKeFile("Barang.txt");
                 return;
             }
         }
@@ -96,6 +131,9 @@ public class ListBarang {
         System.out.println("Barang dengan kode " + kode + " tidak ditemukan.");
     }
 
+    /**
+     * Menampilkan informasi tentang semua barang dalam daftarBarang.
+     */
     public void tampilBarang() {
         if (daftarBarang.isEmpty()) {
             System.out.println("Daftar Barang kosong.");
@@ -108,5 +146,20 @@ public class ListBarang {
                         ", Stok: " + barang.getStokBarang());
             }
         }
-    }    
+    }
+
+    /**
+     * Mencari barang dalam daftarBarang berdasarkan kode barang.
+     *
+     * @param kodeBarang Kode barang yang akan dicari.
+     * @return Objek Barang yang ditemukan, atau null jika tidak ditemukan.
+     */
+    public Barang cariBarang(String kodeBarang) {
+        for (Barang barang : daftarBarang) {
+            if (barang.getKodeBarang().equals(kodeBarang)) {
+                return barang;
+            }
+        }
+        return null;
+    }
 }
