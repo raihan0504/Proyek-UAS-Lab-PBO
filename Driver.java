e5f2befee3d0183787d4df47e72fb8bce205d50a
dellarahmatika <128202610+dellara22@users.noreply.github.com>
2023-12-03 20:03:51 +0700

    Update Driver.java
    nambah javadoc

--- Driver.java
+++ Driver.java
@@ -1,3 +1,7 @@
+/**
+ * Kelas abstrak Driver adalah kelas dasar untuk menggambarkan fungsionalitas pengemudi dalam sistem.
+ * Kelas ini memiliki metode abstrak Menu yang diimplementasikan oleh kelas turunannya.
+ */
 public abstract class Driver {
     
     public abstract void Menu();
