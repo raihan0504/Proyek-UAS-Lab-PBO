dd37d342ea8f3b602cad4ef509ac44e79a8e6050
dellarahmatika <128202610+dellara22@users.noreply.github.com>
2023-12-03 19:48:31 +0700

    Update Cod.java
    nambah javadoc

--- Cod.java
+++ Cod.java
@@ -1,5 +1,14 @@
+/**
+ * Kelas Cod merupakan implementasi dari metode pembayaran "Cash On Delivery" (COD),
+ * yang memungkinkan pembayaran dilakukan saat barang diterima oleh pembeli.
+ * Kelas ini mengextends kelas MetodeBayar dan mengimplementasikan logika pembayaran COD.
+ */
 public class Cod extends MetodeBayar{
 
+    /**
+     * Implementasi metode pembayaran menggunakan Cash On Delivery (COD).
+     * Menampilkan pesan yang menggambarkan proses pembayaran COD.
+     */
     @Override
     public void bayar() {
         // Implement the Cash On Delivery payment logic here
