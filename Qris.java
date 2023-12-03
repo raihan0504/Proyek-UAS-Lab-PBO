5d194f2ce4a900777ec678bdce1b2cd171d4ca4b
raihan0504 <124858485+raihan0504@users.noreply.github.com>
2023-12-03 16:02:40 +0700

    Create Qris.java

---
+++ Qris.java
@@ -0,0 +1,9 @@
+public class Qris extends MetodeBayar {
+
+    @Override
+    public void bayar() {
+        // Implement the QRIS payment logic here
+        System.out.println("Processing QRIS payment...");
+        // Add any specific implementation details for QRIS payment
+    }
+}
