f6982cc184375afee1de092fa256ce38772e7c38
raihan0504 <124858485+raihan0504@users.noreply.github.com>
2023-12-04 01:32:05 +0700

    Create Cod.java

---
+++ Cod.java
@@ -0,0 +1,14 @@
+public class Cod extends MetodeBayar {
+
+    /**
+     * Implementasi metode bayar untuk pembayaran Cash On Delivery (COD).
+     * Menampilkan pesan bahwa proses pembayaran COD sedang diproses dan
+     * memungkinkan penambahan detail implementasi spesifik untuk pembayaran COD.
+     */
+    @Override
+    public void bayar() {
+        // Implementasi logika pembayaran Cash On Delivery (COD) di sini
+        System.out.println("Processing Cash On Delivery payment...");
+        // Tambahkan detail implementasi spesifik untuk pembayaran COD jika diperlukan
+    }
+}
