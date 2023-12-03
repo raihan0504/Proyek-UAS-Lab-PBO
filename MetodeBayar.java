33aa3b1969eef75df94c4907d1d201ddc0402f7b
dellarahmatika <128202610+dellara22@users.noreply.github.com>
2023-12-03 20:28:52 +0700

    Update MetodeBayar.java
    javadoc

--- MetodeBayar.java
+++ MetodeBayar.java
@@ -1,3 +1,9 @@
+/**
+ * Kelas abstrak MetodeBayar menyediakan metode abstrak untuk melakukan proses pembayaran.
+ * Kelas ini dirancang untuk menjadi dasar bagi berbagai jenis metode pembayaran yang ada.
+ * Setiap implementasi MetodeBayar yang konkret harus menyediakan implementasi khusus dari metode bayar().
+ * Sebagai kelas abstrak, tidak dapat diinstansiasi secara langsung.
+ */
 public abstract class MetodeBayar {
 
     public abstract void bayar();
