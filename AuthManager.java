c493bf49fa6317c6789597225e9918ba7da613f3
raihan0504 <124858485+raihan0504@users.noreply.github.com>
2023-12-04 01:28:19 +0700

    Create AuthManager.java

---
+++ AuthManager.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class AuthManager {
+    /**
+     * Variabel status login admin.
+     */
+    private static boolean adminLoggedIn = false;
+
+    /**
+     * Metode untuk melakukan login sebagai admin.
+     * Menggunakan Scanner untuk menerima input username dan password.
+     */
+    public static void loginAdmin() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Username: ");
+        String username = scanner.next();
+        System.out.print("Password: ");
+        String password = scanner.next();
+
+        // Logika otentikasi admin (gantilah dengan logika otentikasi yang sesuai)
+        if (isValidAdmin(username, password)) {
+            adminLoggedIn = true;
+            System.out.println("Login berhasil sebagai admin.");
+        } else {
+            System.out.println("Login gagal. Cek kembali username dan password.");
+        }
+    }
+
+    /**
+     * Metode untuk melakukan logout sebagai admin.
+     * Mengubah status login admin menjadi false.
+     */
+    public static void logoutAdmin() {
+        adminLoggedIn = false;
+        System.out.println("Logout berhasil dari akun admin.");
+    }
+
+    /**
+     * Metode untuk memeriksa apakah admin sudah login.
+     *
+     * @return True jika admin sudah login, False jika belum.
+     */
+    public static boolean isAdminLoggedIn() {
+        return adminLoggedIn;
+    }
+
+    /**
+     * Logika otentikasi admin (contoh sederhana, gantilah dengan logika yang sesuai).
+     *
+     * @param username Username yang dimasukkan untuk login.
+     * @param password Password yang dimasukkan untuk login.
+     * @return True jika otentikasi berhasil, False jika gagal.
+     */
+    private static boolean isValidAdmin(String username, String password) {
+        return "admin".equals(username) && "admin123".equals(password);
+    }
+}
