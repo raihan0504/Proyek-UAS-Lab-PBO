45ffdb80670e745ea7421bd9472d2fa6b70801b0
raihan0504 <124858485+raihan0504@users.noreply.github.com>
2023-12-04 01:29:09 +0700

    Create Bank.java

---
+++ Bank.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Bank extends MetodeBayar {
+
+    /**
+     * Variabel untuk menyimpan nama bank.
+     */
+    private String bankName;
+
+    /**
+     * Variabel untuk menyimpan nomor rekening.
+     */
+    private String accountNumber;
+
+    /**
+     * Objek Scanner untuk menerima input dari pengguna.
+     */
+    Scanner scan = new Scanner(System.in);
+
+    /**
+     * Konstruktor kelas Bank dengan parameter nama bank dan nomor rekening.
+     *
+     * @param bankName       Nama bank untuk diatur.
+     * @param accountNumber  Nomor rekening untuk diatur.
+     */
+    public Bank(String bankName, String accountNumber) {
+        this.bankName = bankName;
+        this.accountNumber = accountNumber;
+    }
+
+    /**
+     * Konstruktor default kelas Bank.
+     */
+    public Bank() {
+        // Tidak melakukan apa-apa pada konstruktor default.
+    }
+
+    /**
+     * Metode bayar yang diimplementasikan dari MetodeBayar.
+     * Meminta input nama bank dan nomor rekening dari pengguna, dan menampilkan informasi pembayaran.
+     */
+    @Override
+    public void bayar() {
+        System.out.print("Masukkan Nama Bank: ");
+        bankName = scan.nextLine();
+        System.out.print("Masukkan No.Rekening: ");
+        accountNumber = scan.nextLine();
+
+        System.out.println("Processing Bank payment...");
+        System.out.println("Bank: " + bankName);
+        System.out.println("Account Number: " + accountNumber);
+    }
+}
