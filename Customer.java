fadd053920c15c4e1f5defe28ed24d1673665276
dellarahmatika <128202610+dellara22@users.noreply.github.com>
2023-12-03 19:51:54 +0700

    Update Customer.java
    nambah javadoc

--- Customer.java
+++ Customer.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList;
 
+/**
+ * Kelas Customer merupakan representasi dari pelanggan dalam sistem, yang dapat
+ * melakukan berbagai tindakan seperti menambahkan transaksi, mengelola keranjang,
+ * dan melihat riwayat transaksi selesai.
+ */
 public class Customer extends Akun {
     private String password;
     private Keranjang keranjang;
@@ -14,7 +19,7 @@ public class Customer extends Akun {
     }
 
     // Konstruktor kedua
-    public Customer(String id) {
+    public Customer(String id) { //@param id ID pelanggan yang akan ditetapkan.
         super(id);
         this.keranjang = new Keranjang();
         this.invoiceSelesai = new ArrayList<Invoice>();
@@ -22,16 +27,16 @@ public class Customer extends Akun {
 
     // Getter untuk password
     public String getPassword() {
-        return password;
+        return password; //@return Kata sandi pelanggan.
     }
 
     // Setter untuk password
-    public void setPassword(String password) {
+    public void setPassword(String password) { //@param password Kata sandi baru untuk pelanggan.
         this.password = password;
     }
 
     // Implementasi metode tambahTransaksi
-    public void tambahTransaksi(Transaksi transaksi) {
+    public void tambahTransaksi(Transaksi transaksi) { //@param transaksi Transaksi yang akan ditambahkan ke daftar invoice selesai
         Invoice invoiceBaru = new Invoice();
         invoiceBaru.setTransaksi(transaksi);
         invoiceBaru.setCustomer(this);
@@ -46,12 +51,12 @@ public class Customer extends Akun {
     // Override dari kelas Akun
     @Override
     public String getId() {
-        return super.getId();
+        return super.getId(); //@return ID pelanggan.
     }
 
     // Override dari kelas Akun
     @Override
-    public void setId(String id) {
+    public void setId(String id) { //@param id ID baru untuk pelanggan.
         super.setId(id);
     }
 
