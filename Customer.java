2881f0450e6f2046e74e7f8a75d1172165f2f7e6
raihan0504 <124858485+raihan0504@users.noreply.github.com>
2023-12-03 15:58:39 +0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,26 +1,58 @@
 import java.util.ArrayList;
 
 public class Customer extends Akun {
+    private String password;
     private Keranjang keranjang;
-    private ArrayList<invoice> invoiceSelesai;
-    
-    public Customer(){
-        super("lijen");
+    private ArrayList<Invoice> invoiceSelesai;
+
+    // Konstruktor pertama
+    public Customer() {
+        super("user");
+        this.password = "user123";
         this.keranjang = new Keranjang();
-        this.invoiceSelesai = new ArrayList<invoice>();
+        this.invoiceSelesai = new ArrayList<Invoice>();
     }
 
-    public Customer(String id){
+    // Konstruktor kedua
+    public Customer(String id) {
         super(id);
         this.keranjang = new Keranjang();
-        this.invoiceSelesai = new ArrayList<invoice>();
+        this.invoiceSelesai = new ArrayList<Invoice>();
     }
 
+    // Getter untuk password
+    public String getPassword() {
+        return password;
+    }
+
+    // Setter untuk password
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // Implementasi metode tambahTransaksi
+    public void tambahTransaksi(Transaksi transaksi) {
+        Invoice invoiceBaru = new Invoice();
+        invoiceBaru.setTransaksi(transaksi);
+        invoiceBaru.setCustomer(this);
+
+        // Tambahkan invoice ke daftar invoice selesai
+        invoiceSelesai.add(invoiceBaru);
+
+        // Kosongkan keranjang setelah transaksi selesai
+        keranjang.clearKeranjang();
+    }
+
+    // Override dari kelas Akun
+    @Override
     public String getId() {
-        return this.id;
+        return super.getId();
     }
 
+    // Override dari kelas Akun
+    @Override
     public void setId(String id) {
-        this.id = id;
+        super.setId(id);
     }
+
 }
