29d747f6291557fbfc7f56a464f4f873001796cb
dellarahmatika <128202610+dellara22@users.noreply.github.com>
2023-12-03 19:39:18 +0700

    Update Bank.java
    menambahkan javadoc

--- Bank.java
+++ Bank.java
@@ -1,17 +1,32 @@
 import java.util.Scanner;
 
+/**
+ * Kelas Bank merupakan implementasi dari metode pembayaran yang menggunakan
+ * transfer bank untuk melakukan pembayaran.
+ */
 public class Bank extends MetodeBayar{
 
     private String bankName;
     private String accountNumber;
     Scanner scan = new Scanner(System.in);
 
+    /**
+     * Konstruktor untuk membuat objek Bank dengan informasi nama bank dan nomor rekening.
+     *
+     * @param bankName       Nama bank untuk pembayaran.
+     * @param accountNumber  Nomor rekening untuk pembayaran.
+     */
     public Bank(String bankName, String accountNumber) {
         this.bankName = bankName;
         this.accountNumber = accountNumber;
     }
 
     @Override
+    /**
+     * Implementasi metode pembayaran menggunakan transfer bank.
+     * Pengguna diminta untuk memasukkan nama bank dan nomor rekening,
+     * kemudian informasi pembayaran ditampilkan.
+     */
     public void bayar() {
         System.out.print("Masukkan Nama Bank: ");
         bankName = scan.nextLine();
