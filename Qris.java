9b370110d5494a8d47c49852b84bef5aec6e0a93
raihan0504 <124858485+raihan0504@users.noreply.github.com>
2023-12-04 01:54:46 +0700

    Create Qris.java

---
+++ Qris.java
@@ -0,0 +1,9 @@
+
+public abstract class MetodeBayar {
+
+    /**
+     * Metode abstrak untuk melakukan proses pembayaran.
+     * Implementasi spesifik dari metode ini harus diberikan oleh kelas turunan.
+     */
+    public abstract void bayar();
+}
