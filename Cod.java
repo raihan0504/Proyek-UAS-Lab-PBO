4d6227a3875c1269403487c0ff325d854124d9e9
raihan0504 <124858485+raihan0504@users.noreply.github.com>
2023-12-03 15:58:06 +0700

    Create Cod.java

---
+++ Cod.java
@@ -0,0 +1,9 @@
+public class Cod extends MetodeBayar{
+
+    @Override
+    public void bayar() {
+        // Implement the Cash On Delivery payment logic here
+        System.out.println("Processing Cash On Delivery payment...");
+        // Add any specific implementation details for COD payment
+    }
+}
