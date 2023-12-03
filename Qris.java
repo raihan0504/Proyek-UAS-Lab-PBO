3b665e5c9c864a8a14b719d211d0769b1ccf5093
dellarahmatika <128202610+dellara22@users.noreply.github.com>
2023-12-03 20:31:05 +0700

    Update Qris.java
    javadoc

--- Qris.java
+++ Qris.java
@@ -1,3 +1,10 @@
+/**
+ * Kelas Qris merupakan implementasi dari kelas abstrak MetodeBayar yang digunakan untuk melakukan pembayaran
+ * menggunakan metode QRIS
+ * Kelas ini menyediakan implementasi konkret dari metode bayar(), yang akan dipanggil saat pelanggan memilih
+ * untuk melakukan pembayaran menggunakan QRIS.
+ * Implementasi ini mencakup logika khusus pembayaran QRIS dan memberikan pesan bahwa pembayaran QRIS sedang diproses.
+ * /
 public class Qris extends MetodeBayar {
 
     @Override
