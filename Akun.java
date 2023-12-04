9785ef2eea790da8c93f985ab45639ffe813744c
raihan0504 <124858485+raihan0504@users.noreply.github.com>
2023-12-04 07:20:25 +0700

    Update Akun.java

--- Akun.java
+++ Akun.java
@@ -1,18 +1,35 @@
 class Akun {
     protected String id;
-
+    /**
+     * Konstruktor default kelas Akun.
+     */
     public Akun(){
 
     }
 
+    /**
+     * Konstruktor kelas Akun dengan parameter ID.
+     *
+     * @param id ID yang akan diatur untuk akun.
+     */
     public Akun(String id){
         this.id = id;
     }
 
+    /**
+     * Mendapatkan ID akun.
+     *
+     * @return String berisi ID akun.
+     */
     public String getId(){
         return id;
     }
 
+    /**
+     * Mengatur ID baru untuk akun.
+     *
+     * @param id ID baru yang akan diatur untuk akun.
+     */
     public void setId(String id) {
         this.id = id;
     }
