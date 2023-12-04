c21ac1881f3309a17117cf970bf37d3395264462
raihan0504 <124858485+raihan0504@users.noreply.github.com>
2023-12-04 07:28:13 +0700

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,4 +1,7 @@
 public abstract class Driver {
-    
+    /**
+     * Metode abstrak yang harus diimplementasikan oleh setiap kelas turunan.
+     * Metode ini digunakan untuk menampilkan menu aksi atau antarmuka pengguna khusus.
+     */
     public abstract void Menu();
 }
