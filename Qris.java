4a7425ddfbefc0c99fdda8accf649a5f986b75e7
raihan0504 <124858485+raihan0504@users.noreply.github.com>
2023-12-04 07:35:02 +0700

    Update Qris.java

--- Qris.java
+++ Qris.java
@@ -1,9 +1,12 @@
 public class Qris extends MetodeBayar {
 
+    /**
+     * Metode untuk melakukan proses pembayaran menggunakan QRIS.
+     * Implementasi spesifik dari metode ini mencakup logika pembayaran QRIS dan
+     * mungkin mencakup detail implementasi khusus untuk pembayaran QRIS.
+     */
     @Override
     public void bayar() {
-        // Implement the QRIS payment logic here
         System.out.println("Processing QRIS payment...");
-        // Add any specific implementation details for QRIS payment
     }
 }
