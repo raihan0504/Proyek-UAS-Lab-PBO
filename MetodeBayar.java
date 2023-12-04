cea9ff3f2fee89b08eb81dbbae377aea08aef5df
raihan0504 <124858485+raihan0504@users.noreply.github.com>
2023-12-04 07:33:51 +0700

    Update MetodeBayar.java

--- MetodeBayar.java
+++ MetodeBayar.java
@@ -1,4 +1,8 @@
 public abstract class MetodeBayar {
 
+    /**
+     * Metode abstrak untuk melakukan proses pembayaran.
+     * Implementasi spesifik dari metode ini harus diberikan oleh kelas turunan.
+     */
     public abstract void bayar();
 }
