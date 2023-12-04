f9717e5cddf0577ba6e2cd124fc461767e51a800
raihan0504 <124858485+raihan0504@users.noreply.github.com>
2023-12-04 07:08:25 +0700

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,11 +1,22 @@
 public class Admin extends Akun {
     private String password;
-    
+
+    /**
+     * Konstruktor default untuk objek Admin. Menginisialisasi ID dengan nilai
+     * "admin" dan kata sandi dengan nilai "admin123".
+     */
     public Admin(){
        super("admin");
        this.password = "admin123";
     }
 
+     /**
+     * Konstruktor parameter untuk objek Admin. Menginisialisasi ID dengan nilai
+     * yang diberikan dan kata sandi dengan nilai yang diberikan.
+     *
+     * @param id       ID untuk akun administrator.
+     * @param password Kata sandi untuk akun administrator.
+     */
     public Admin(String id,String password){
         super(id);
         this.password = password;
